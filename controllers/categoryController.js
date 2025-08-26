82f29a5e3b096c6094e0078076d48add02ad3187
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-08-27 01:58:13 +0530

    Add index controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,21 @@
-exports.create = (req, res) => {
+const { insertCategory, getCategories } = require("../db/query");
+
+function create(req, res) {
   res.render("createCategory");
+}
+
+async function postCategory(req, res) {
+  await insertCategory(req.body.categoryName);
+  res.redirect("/category");
+}
+
+async function showCategory(req, res) {
+  const data = await getCategories();
+  res.render("index", { value: "category", categoryData: data });
+}
+
+module.exports = {
+  create,
+  postCategory,
+  showCategory,
 };
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,3 +1,27 @@
-exports.create = (req, res) => {
-  res.render("createItem");
+const { getCategories, insertItem, getItems } = require("../db/query");
+
+async function create(req, res) {
+  const data = await getCategories();
+  res.render("createItem", { categories: data });
+}
+
+async function postItems(req, res) {
+  await insertItem(req.body.itemName, req.body.itemCategory);
+  res.redirect("/item");
+}
+
+async function showItem(req, res) {
+  const data = await getItems();
+  const categories = await getCategories();
+  res.render("index", {
+    value: "item",
+    categories: categories,
+    itemData: data,
+  });
+}
+
+module.exports = {
+  create,
+  postItems,
+  showItem
 };
