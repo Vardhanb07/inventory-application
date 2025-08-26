26c1bb92f5a338189e83dd2319a1a48462db585d
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-08-27 01:55:35 +0530

    Remove unused functions

--- db/query.js
+++ db/query.js
@@ -5,11 +5,6 @@ async function getCategories() {
   return rows;
 }
 
-async function getCategory(id) {
-  const { rows } = await pool.query("SELECT * FROM category WHERE id=$1", [id]);
-  return rows;
-}
-
 async function insertCategory(name) {
   await pool.query("INSERT INTO category (name) VALUES ($1)", [name]);
 }
@@ -19,14 +14,16 @@ async function getItems() {
   return rows;
 }
 
-async function getItem(id) {
-  const { rows } = await pool.query("SELECT * FROM item WHERE id=$1", [id]);
-  return rows;
-}
-
 async function insertItem(name, categoryId) {
   await pool.query("INSERT INTO item (name, category_id) VALUES ($1, $2)", [
     name,
     categoryId,
   ]);
 }
+
+module.exports = {
+  getCategories,
+  insertCategory,
+  getItems,
+  insertItem,
+};
